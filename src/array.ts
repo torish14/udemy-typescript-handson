385297c4e72195655994e12b113ba3b6dba9a352
torish14 <nanamin.104@icloud.com>
2022-04-08 12:45:30 +0900

    ✨ feat: created array.ts and wrote array type sample code

---
+++ src/array.ts
@@ -0,0 +1,14 @@
+export {}
+
+let numbers: number[] = [1, 2, 3]
+
+let numbers2: Array<number> = [1, 2, 3]
+let strings2: Array<string> = ['a', 'b', 'c']
+
+let twoDemensionalArray: number[][] = [
+  [10, 20],
+  [30, 40],
+]
+
+let array: (number | boolean | string)[] = [1, false, 'Japan']
+let array2: Array<number | boolean | string> = [2, true, 'Korea']
