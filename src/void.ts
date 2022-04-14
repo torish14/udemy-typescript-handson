2e2988943d6781565fecc53d2d9a7d82eff85673
torish14 <nanamin.104@icloud.com>
2022-04-14 14:17:24 +0900

    🐞 fix: converted undefined type to void type

--- src/void.ts
+++ src/void.ts
@@ -2,9 +2,8 @@ export {}
 
 // 関数式
 // 関数宣言よりもスコープや再宣言が厳密
-function returnNothing(): undefined {
+function returnNothing(): void {
   console.log('I dont return anything!')
-  return undefined
 }
 
 console.log(returnNothing())
