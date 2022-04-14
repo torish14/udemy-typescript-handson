88c3ca03ad8afb1a9efb0b6e9d331b394b254b86
torish14 <nanamin.104@icloud.com>
2022-04-14 14:12:13 +0900

    ✨ feat: added undefined type and returned it

--- src/void.ts
+++ src/void.ts
@@ -2,8 +2,9 @@ export {}
 
 // 関数式
 // 関数宣言よりもスコープや再宣言が厳密
-function returnNothing() {
+function returnNothing(): undefined {
   console.log('I dont return anything!')
+  return undefined
 }
 
 console.log(returnNothing())
