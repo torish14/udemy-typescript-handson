257eb0e1d3b42ee15dacaf418ed46267791669e5
torish14 <nanamin.104@icloud.com>
2022-04-17 10:50:02 +0900

    ✨ feat: added a sample code about never type

---
+++ src/never.ts
@@ -0,0 +1,16 @@
+export {}
+
+function error(message: string): never {
+  throw new Error(message)
+}
+
+try {
+  let result = error('Something went wrong!')
+  console.log(result)
+} catch (e) {
+  console.log(e)
+}
+
+let foo: void = undefined
+// never には、undefined も null も許されない
+let bar: never = undefined
